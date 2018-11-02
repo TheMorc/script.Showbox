c928a316caf476f5971c9829fa9f6ff8aaf91222
Morc (Richard Gráčik) <r.gracik@gmail.com>
2018-11-03 00:29:29 +0100

    v3.5 added spinner

--- script.py
+++ script.py
@@ -1,10 +1,15 @@
 import xbmc
 import sys
 if sys.argv[0] == '':
-    xbmc.executebuiltin('Notification(Showbox,It doesn\'t do anything here.)')
+    xbmc.executebuiltin('Notification(Showbox,Actually, it doesn\'t do anything here for now, but i think it will do in the future.)')
 elif sys.argv[1] == 'toggle':
+    xbmc.executebuiltin('ActivateWindow(busydialognocancel)')
     if xbmc.Player().isPlaying() == 1:
         xbmc.executebuiltin('Stop')
         xbmc.executebuiltin('PlayerControl(Stop)')
+    xbmc.executebuiltin('ActivateWindow(busydialognocancel)')
     xbmc.executebuiltin('CECToggleState')
     xbmc.executebuiltin('ActivateScreensaver')
+    xbmc.sleep(1000)
+    xbmc.executebuiltin('Dialog.Close(busydialognocancel)')
+    
