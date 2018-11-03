8a25cf1e33c158b44f8892fb48d8d652baa6a313
Morc (Richard Gráčik) <r.gracik@gmail.com>
2018-11-03 09:01:07 +0100

    Update script.py

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ elif sys.argv[1] == 'toggle':
     if xbmc.Player().isPlaying() == 1:
         xbmc.executebuiltin('Stop')
         xbmc.executebuiltin('PlayerControl(Stop)')
+        xbmc.sleep(250)
     xbmc.executebuiltin('ActivateWindow(busydialognocancel)')
     xbmc.sleep(500)
     xbmc.executebuiltin('CECToggleState')
